2daad13dfa084ebdbd70d672d74db17acecf0c27
Сеймур Мамедов <146606145+Seymur-m@users.noreply.github.com>
2024-10-01 19:57:26 +0300

    first commit

---
+++ src/main/java/com/example/demo1/service/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.demo1.service;
+
+import com.example.demo1.dao.UserDao;
+import com.example.demo1.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDAO;
+
+    @Override
+    public void save(User user) {
+        userDAO.save(user);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userDAO.findAll();
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userDAO.findById(id);
+    }
+
+    @Override
+    public void update(User user) {
+        userDAO.update(user);
+    }
+
+    @Override
+    public void delete(Long id) {
+        userDAO.delete(id);
+    }
+}
